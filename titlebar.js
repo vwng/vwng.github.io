c2dda1b8178f4eff268c25b466dfcea7f39e3850
ava <avaw731@gmail.com>
2018-07-30 21:39:13 -0400

    Update

---
+++ titlebar.js
@@ -0,0 +1,63 @@
+$('#maincontent').mouseover(function(){
+	$('#titlebar').text("INDEX TEXT");
+    $('#titlebar').show();
+});
+
+$('#maincontent').mouseout(function(){
+    $('#titlebar').hide();
+});
+	 
+$('#name').mouseover(function(){
+	$('#titlebar').text("NAMEEE");
+    $('#titlebar').show();
+});
+	 
+$('#name').mouseout(function(){
+    $('#titlebar').hide();
+});
+
+$('#blue').mouseover(function(){
+	$('#titlebar').text("Blue");
+    $('#titlebar').show();
+});
+
+$('#blue').mouseout(function(){
+    $('#titlebar').hide();
+});
+	 
+$('#cyrd').mouseover(function(){
+	$('#titlebar').text("Can you read this?");
+    $('#titlebar').show();
+});
+	 
+$('#cyrd').mouseout(function(){
+    $('#titlebar').hide();
+});
+	
+$('#consider').mouseover(function(){
+	$('#titlebar').text("Consider the Link");
+    $('#titlebar').show();
+});
+	 
+$('#consider').mouseout(function(){
+    $('#titlebar').hide();
+});
+	
+$('#milk').mouseover(function(){
+	$('#titlebar').text("Milk Packaging");
+    $('#titlebar').show();
+});
+	 
+$('#milk').mouseout(function(){
+    $('#titlebar').hide();
+});
+	
+$('#frenzy').mouseover(function(){
+	$('#titlebar').text("Feeding Frenzy");
+    $('#titlebar').show();
+});
+	 
+$('#frenzy').mouseout(function(){
+    $('#titlebar').hide();
+		
+});// JavaScript Document
